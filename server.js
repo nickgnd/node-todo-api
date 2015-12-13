69c3dc820d2b23801469c581140a438745a9cc5c
nickgnd <nick.gnd@gmail.com>
2015-12-13 16:03:44 +0100

    Add underscore library and refactor #show and #create action

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 var express = require('express');
-var findElement = require('./findElement.js');
+var bodyParser = require('body-parser'); // => middleware
+// var findElement = require('./findElement.js');
+
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-  id: 1,
-  description: 'Cooks for everybody',
-  completed: false
-}, {
-  id: 2,
-  description: 'Buy chicken and senape',
-  completed: false
-}, {
-  id: 3,
-  description: 'Clean the kitchen',
-  completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+// every Json request comes in is parsed by express
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.send("Todo API Root");
@@ -29,14 +25,31 @@ app.get('/todos', function(req, res) {
 // GET - /todos/:id
 app.get('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
 
-  findElement(todoId, todos).then(function(todo) {
-    res.json(todo);
-  }).catch(function(e) {
-    console.log(e);
+  if (matchedTodo) {
+    res.json(matchedTodo);
+  } else {
     res.status(404).send();
-  });
+  }
 
+});
+
+// POST - /todos
+app.post('/todos', function(req, res) {
+  body = req.body;
+
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    return res.status(400).send();
+  }
+
+
+
+  body.id = todoNextId++; // before set body.id equal to todoNextId and then increment it
+  todos.push(body);
+
+  // res.status(201).send();
+  res.json(body);
 });
 
 app.listen(PORT, function () {
